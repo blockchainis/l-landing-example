df13508d1a2a15d00d0e9eae2e3cd2d211d0912b
hoon2 <sanghoon.engineer@gmail.com>
2022-11-05 14:55:17 +0900

    전역 배경색상 적용 완료

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "black",
+        color: "white",
+      },
+    },
+  },
   fonts: {
     body: `'Nanum Gothic', sans-serif`,
   },
