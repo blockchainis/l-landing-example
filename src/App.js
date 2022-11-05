a35e2813043e2555bb59476276927be535915601
hoon2 <sanghoon.engineer@gmail.com>
2022-11-05 15:56:11 +0900

    hero example 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "@fontsource/nanum-gothic";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import Hero from "@components/Hero";
+import HeroTwo from "@components/HeroTwo";
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Hero />
+      <HeroTwo />
     </ChakraProvider>
   );
 }
--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -10,7 +10,7 @@ import {
   createIcon,
 } from "@chakra-ui/react";
 
-export default function CallToActionWithAnnotation() {
+export default function HeroTwo() {
   return (
     <>
       <Container maxW={"3xl"}>
@@ -25,15 +25,15 @@ export default function CallToActionWithAnnotation() {
             fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
             lineHeight={"110%"}
           >
-            Make money from <br />
-            <Text as={"span"} color={"green.400"}>
-              your audience
+            비밀을 담아주세요 <br />
+            <Text as={"span"} color={"purple.400"}>
+              오직 당신만의
             </Text>
           </Heading>
-          <Text color={"gray.500"}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
+          <Text color={"gray.200"}>
+            항상 마음속으로만 간직한 이야기 하지만 기억하고 싶었던 이야기 누군가
+            볼까 두려웠던 속마음의 이야기 이제는 비담에 담아주시면 됩니다. 어느
+            누구도 볼 수 없습니다. 오로지 당신만 볼 수 있습니다.
           </Text>
           <Stack
             direction={"column"}
@@ -43,18 +43,17 @@ export default function CallToActionWithAnnotation() {
             position={"relative"}
           >
             <Button
-              colorScheme={"green"}
-              bg={"green.400"}
+              bg={"purple.400"}
               rounded={"full"}
               px={6}
               _hover={{
-                bg: "green.500",
+                bg: "purple.500",
               }}
             >
-              Get Started
+              비담 시작하기
             </Button>
-            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-              Learn more
+            <Button variant={"link"} colorScheme={"purple"} size={"sm"}>
+              비담 알아보기
             </Button>
           </Stack>
         </Stack>
