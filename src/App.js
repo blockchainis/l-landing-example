2e5c1325c081ddfcddda30f0003f996555d89fc1
hoon2 <sanghoon.engineer@gmail.com>
2022-11-05 22:27:17 +0900

    feature개발

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "@fontsource/nanum-gothic";
 import { ChakraProvider } from "@chakra-ui/react";
 import Hero from "@components/Hero";
 import HeroTwo from "@components/HeroTwo";
+import Features from "@components/Features";
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Hero />
       <HeroTwo />
+      <Features />
     </ChakraProvider>
   );
 }
